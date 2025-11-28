9bb58c8e5784dcbc50eabcd8821e3bf6371fa5dd
Abdelouahab EL HILALI <elhilali.abdelouahab@gmail.com>
2025-11-29 00:22:21 +0100

    Fonctionnalité : Réinitialisation MDP

--- Backend/controllers/AuthentificationController.js
+++ Backend/controllers/AuthentificationController.js
@@ -1,13 +1,26 @@
 const Users = require("../models/users");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const nodemailer = require("nodemailer");
 /**
  * Création du token 
  * @param {*} id 
  * @returns 
  */
 const createToken = (id) => jwt.sign({ id }, "RANDOM_TOKEN_SECRET", { expiresIn: "2h" });
+
+
+    /**
+     * Conf de transporteur
+     */
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'elhilali.abdelouahab@gmail.com',
+        pass: 'fikhmgcheswzhbft'
+      }
+    });
+
 
 /**
  * Connexion de l'utilisateur 
@@ -60,3 +73,214 @@ module.exports.logoutUser = (req, res) => {
     return res.status(500).json({ error: "Une erreur est survenue lors de la déconnexion." });
   }
 };
+
+/** Demande de réinitialisation du mot de passe - à implémenter
+ * @param {*} req 
+ * @param {*} res
+ * @returns
+ */
+  
+module.exports.demandeReinitialisation = async (req, res) => {
+  try {
+    // R2cupérer l'email de l'utilisateur
+    var checkUser = await Users.findOne({ where: { email: req.body.email } });
+    if (!checkUser) {
+      return res.status(404).json({ error: "Aucun utilisateur trouvé avec cet email." });
+    }
+
+    // Générer un code de réinitialisation
+    const codeReinitialisation = Math.floor(100000 + Math.random() * 900000).toString();
+    const dateExpirationCode = new Date(Date.now() +  5 * 60 * 1000); 
+    
+    // Modifier l'utilisateur avec le code et la date d'expiration
+    checkUser.codeReinitialisation = codeReinitialisation;
+    checkUser.dateExpirationCode = dateExpirationCode;
+    await checkUser.save();
+
+
+    // Envoyer le code par email
+    const htmlEmail = `<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Confirmation de compte</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f5f7fa;
+        margin: 0;
+        padding: 0;
+      }
+
+      .container {
+        max-width: 600px;
+        margin: 40px auto;
+        background: #ffffff;
+        border-radius: 12px;
+        padding: 30px;
+        box-shadow: 0 6px 20px rgba(0, 0, 0, 0.08);
+        border: 1px solid #e5e7eb;
+      }
+
+      .header {
+        text-align: center;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #eee;
+      }
+
+      .header h1 {
+        font-size: 24px;
+        color: #5b7cff;
+        margin: 0;
+        text-transform: lowercase;
+      }
+
+      .content {
+        font-size: 15px;
+        color: #333;
+        line-height: 1.6;
+        padding: 20px 0;
+      }
+
+      .btn {
+        display: inline-block;
+        background: linear-gradient(135deg, #5b7cff, #8b5cf6);
+        color: #fff !important;
+        padding: 12px 28px;
+        border-radius: 8px;
+        text-decoration: none;
+        font-weight: bold;
+        margin: 25px 0;
+      }
+
+      .footer {
+        text-align: center;
+        font-size: 12px;
+        color: #777;
+        padding-top: 20px;
+        border-top: 1px solid #eee;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="header">
+        <h1>loominetfy</h1>
+      </div>
+
+      <div class="content">
+        <p>
+          Bonjour <strong>${checkUser.prenom}</strong>,<br /><br/>
+          Vous avez demandé la réinitialisation de votre mot de passe sur <strong>Loominetfy</strong> 🎉<br />
+          Pour finaliser votre réinitialisation, veuillez utiliser le code ci-dessous :
+        </p>
+
+        <div style="text-align:center;">
+          <a href="#" class="btn" target="_blank" rel="noopener noreferrer">
+            Code : <strong>${codeReinitialisation}</strong>
+          </a>
+        </div>
+
+        <p>
+        Ce code est valide pendant 5 minutes.
+        Si vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer cet
+        email en toute sécurité.
+        </p>
+      </div>
+    </div>
+  </body>
+</html>`;
+
+    const mailOptions = {
+      from: 'loominetfy@gmail.com',
+      to: checkUser.email,
+      subject: 'Réinitialisation du mot de passe sur Loominetfy',
+      html: htmlEmail
+    };
+
+    await transport.sendMail(mailOptions);
+
+    return res.status(200).json({ message: "Code de réinitialisation envoyé à l'email." });
+  } catch (error) {
+    console.error("Erreur lors de l'envoi du code de réinitialisation :", error);
+    return res.status(500).json({ error: "Une erreur est survenue lors de l'envoi du code de réinitialisation." });
+  }
+}
+
+/** Vérifier le code de réinitialisation du mot de passe 
+ * @param {*} req 
+ * @param {*} res
+ * @returns
+ */
+
+module.exports.checkCodeReinitialisation = async (req, res) => {
+  try {
+    // Vérifier l'email et le code et la date d'expiration
+    const checkUser = await Users.findOne({ where: { email: req.body.email } });
+
+    if (!checkUser) {
+      return res.status(404).json({ error: "Aucun utilisateur trouvé avec cet email." });
+    }
+
+    if (checkUser.codeReinitialisation !== req.body.codeReinitialisation) {
+      return res.status(400).json({ error: "Code de réinitialisation invalide." });
+    }
+    if (new Date() > checkUser.dateExpirationCode) {
+      return res.status(400).json({ error: "Le code de réinitialisation a expiré." });
+    } 
+
+    return res.status(200).json({ message: "Code de réinitialisation valide." });
+
+  } catch (error) {
+    console.error("Erreur lors de la vérification du code de réinitialisation :", error);
+    return res.status(500).json({ error: "Une erreur est survenue lors de la vérification du code de réinitialisation." });
+  }
+}
+
+
+/** Réinitialisation du mot de passe
+ * @param {*} req 
+ * @param {*} res
+ * @returns
+ */
+
+module.exports.resetPasswordUser = async (req, res) => {
+  try {
+    // Vérifier l'email
+    const checkUser = await Users.findOne({ where: { email: req.body.email } });
+
+    if (!checkUser) {
+      return res.status(404).json({ error: "Aucun utilisateur trouvé avec cet email." });
+    }
+
+    
+
+    // Vérifier si le nouveau mot de passe est différent de l'ancien
+    const oldPassword = await bcrypt.compare(req.body.newPassword, checkUser.password);
+    if (oldPassword) {
+      return res.status(400).json({ error: "Le nouveau mot de passe doit être différent de l'ancien." });
+    }
+
+    // Vérifier le format du nouveau mot de passe
+    const regexPassword =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!regexPassword.test(req.body.newPassword)) {
+      return res.status(400).json({ error: "Le nouveau mot de passe ne respecte pas les critères de sécurité." });
+    }
+    
+    // Hacher le nouveau mot de passe
+    const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
+    checkUser.password = hashedPassword;
+    // Supprimer le code de réinitialisation et la date d'expiration
+    checkUser.codeReinitialisation = null;
+    checkUser.dateExpirationCode = null;
+    await checkUser.save();
+
+    return res.status(200).json({ message: "Mot de passe réinitialisé avec succès." });
+  } catch (error) {
+    console.error("Erreur lors de la réinitialisation du mot de passe :", error);
+    return res.status(500).json({ error: "Une erreur est survenue lors de la réinitialisation du mot de passe." });
+  }
+}
--- Frontend/src/app/component/reset-password/reset-password.component.ts
+++ Frontend/src/app/component/reset-password/reset-password.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import Swal from 'sweetalert2';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment.dev';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -13,10 +16,12 @@ export class ResetPasswordComponent {
   resetForm: FormGroup;
   currentStep = 1; // 1: choix + envoi, 2: code, 3: nouveau mdp
   success = false;
-
+  remainingTime: number = 0;
+  isDisabled: boolean = false;
+  interval: any;
   errors: { [key: string]: string } = {};
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
     this.resetForm = this.fb.group({
       method: ['email', Validators.required],
       email: [''],
@@ -53,9 +58,9 @@ export class ResetPasswordComponent {
     this.clearErrors();
     const method = this.methodControl.value;
     let ok = true;
-
+    const email = this.resetForm.get('email')?.value?.trim();
     if (method === 'email') {
-      const email = this.resetForm.get('email')?.value?.trim();
+
       if (!email) {
         this.errors['email'] = "L'email est obligatoire.";
         ok = false;
@@ -73,23 +78,32 @@ export class ResetPasswordComponent {
 
     if (!ok) return;
 
+    // Appel API
+    this.http.post(`${environment.apiUrl}/auth/forgotpassword`, { email}).subscribe({
+        next: () => {
+           // Passage à l’étape 2
+           this.currentStep = 2;
+           this.success = false;
+           this.clearErrors();
+           this.startTimer();
+        },
+        error: (err: HttpErrorResponse) => {
+          console.error('Erreur lors de l\'envoi du code de vérification', err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Erreur',
+            text: err.error.error || 'Une erreur est survenue lors de l\'envoi du code.',
+          });
+        },
+      });
     // TODO: appel API pour envoyer le code
-    console.log('Envoi du code de vérification...', this.resetForm.value);
+    // console.log('Envoi du code de vérification...', this.resetForm.value);
 
-    Swal.fire({
-      icon: 'success',
-      title: 'Code envoyé',
-      text:
-        this.methodControl.value === 'email'
-          ? 'Un code de vérification a été envoyé à votre adresse email.'
-          : 'Un code de vérification a été envoyé par SMS.',
-      showConfirmButton: false,
-      timer: 1500
-    }).then(() => {
-      // Passage à l’étape 2
-      this.currentStep = 2;
-      this.success = false;
-    });
+
+  }
+
+  resendCode() {
+    this.sendCode();
   }
 
   backToStep1() {
@@ -108,6 +122,7 @@ export class ResetPasswordComponent {
   verifyCode() {
     this.clearErrors();
     const code = this.resetForm.get('code')?.value?.trim();
+    const email = this.resetForm.get('email')?.value?.trim();
     let ok = true;
 
     if (!code || code.length < 4) {
@@ -119,12 +134,24 @@ export class ResetPasswordComponent {
 
     // TODO: appel API pour vérifier le code
     console.log('Vérification du code...', code);
+    this.http.post(`${environment.apiUrl}/auth/checkcode`, { email, codeReinitialisation: code }).subscribe({
+        next: () => {
+           // Passage à l’étape 3
+            this.currentStep = 3;
+            this.success = false;
+            this.clearErrors();
+        },
+        error: (err: HttpErrorResponse) => {
+          console.error('Erreur lors de l\'envoi du code de vérification', err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Erreur',
+            text: err.error.error || 'Une erreur est survenue lors de la vérification du code.',
+          });
+        },
+      });
 
 
-      // Passage à l’étape 3
-      this.currentStep = 3;
-      this.success = false;
-      this.clearErrors();
   }
 
   // Étape 3 : nouveau mot de passe
@@ -133,7 +160,7 @@ export class ResetPasswordComponent {
 
     this.clearErrors();
     this.success = false;
-
+    const email = this.resetForm.get('email')?.value?.trim();
     const newPassword = this.resetForm.get('newPassword')?.value;
     const confirmPassword = this.resetForm.get('confirmPassword')?.value;
 
@@ -159,19 +186,61 @@ export class ResetPasswordComponent {
     if (!ok) return;
 
     // TODO: appel API pour changer le mot de passe
-    console.log('Mot de passe réinitialisé', this.resetForm.value);
+        this.http.put(`${environment.apiUrl}/auth/resetpassword`, { email, newPassword }).subscribe({
+        next: () => {
+          Swal.fire({
+           icon: 'success',
+          title: 'Mot de passe réinitialisé',
+          text: 'Votre mot de passe a été modifié avec succès.',
+          confirmButtonText: 'Retour à la connexion',
+          confirmButtonColor: '#5b7cff'
+          }).then(() => {
+            this.success = true;
+            this.router.navigate(['/auth']);
+          });
+        },
+        error: (err: HttpErrorResponse) => {
+          console.error(err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Erreur',
+            text: err.error.error || 'Une erreur est survenue lors de la vérification du code.',
+          });
+        },
+      });
 
     this.success = true;
-
-    Swal.fire({
-      icon: 'success',
-      title: 'Mot de passe réinitialisé',
-      text: 'Votre mot de passe a été modifié avec succès.',
-      confirmButtonText: 'Retour à la connexion',
-      confirmButtonColor: '#5b7cff'
-    }).then(() => {
-      console.log('Redirection vers la page de login');
-      // this.router.navigate(['/login']);
-    });
   }
+
+  navigateTo() {
+    this.router.navigate(['/auth']);
+  }
+
+  startTimer() {
+  this.remainingTime = 5 * 60;  // 5 minutes
+  this.isDisabled = true;
+
+  if (this.interval) {
+    clearInterval(this.interval);
+  }
+
+  this.interval = setInterval(() => {
+    this.remainingTime--;
+
+    if (this.remainingTime <= 0) {
+      clearInterval(this.interval);
+      this.isDisabled = false;
+    }
+  }, 1000);
+  }
+
+  formatTime(seconds: number): string {
+  const m = Math.floor(seconds / 60);
+  const s = seconds % 60;
+
+  const mm = m < 10 ? '0' + m : m;
+  const ss = s < 10 ? '0' + s : s;
+
+  return `${mm}:${ss}`;
+}
 }
